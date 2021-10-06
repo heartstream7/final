6af5b6469ebe68cfd5b2ac677b587678b332d1db
heartstream7 <91556106+heartstream7@users.noreply.github.com>
2021-10-06 17:57:15 +0800

    added a file

---
+++ project shield.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void function0(int* p1, int* p2)
+{
+	int temp;
+	temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+int main()
+{
+	int a = 100;
+	int b = 1000;
+	function0(&a, &b);
+	cout << a << endl;
+	cout << b << endl;
+	system("pause");
+	return 0;
+}
